0a5ca7d171cfb6347ec8e8370c4876cde869aeb8
BecketTocci <becket@tocci.org>
2025-06-10 15:37:25 -0500

    Fix bugs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from os import getcwd,mkdir
 final_name=str()
 name=str()
 save_dir=str()
-
+save_dir_empty=False
 
 m=MakeMKV(0)
 
@@ -22,11 +22,13 @@ def get_input():
     global final_name
     global name
     global save_dir
+    global save_dir_empty
     name=input("What is the name of this media? ")
     year=input("What year was this media made in? ")
     save_dir=input("Where would you like it to be stored? (Defaults to current directory)")
     if save_dir=="":
-        save_dir=getcwd()+"/RippedMedia"
+        save_dir=getcwd()
+        save_dir_empty=True
     final_name=name + " (" + year + ")"
     con()
 
@@ -35,7 +37,9 @@ print("Getting title count...")
 info=m.info()
 print("Title count is "+ str(info["title_count"]))
 
-try: mkdir(save_dir+"/"+final_name)
+try: mkdir(save_dir+"/"+"RippedMedia")
+except:pass
+try: mkdir(save_dir+"/"+"RippedMedia"+final_name)
 except:print("Directory already found. Warning that if you don't delete already ripped tracks in there it will try to overwrite them and error.")
 for i in range(info["title_count"]):
     print("Ripping title " + str(i))
