84d03d083857ecc488ac7cd0fe541d7b9a7f5742
BecketTocci <becket@tocci.org>
2025-06-24 16:00:41 -0500

    Better formatting + comments

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from makemkv import *
-from os import getcwd,mkdir
+from makemkv import * # Python wrapper for MakeMKV
+from os import getcwd,mkdir # This way we can store the media in their own folders, see line 27.
 
 final_name=str()
 name=str()
 save_dir=str()
 save_dir_empty=False
 
-m=MakeMKV(0)
+m=MakeMKV(0) # Starts a MakeMKV session on disk drive 0.
 
 def con():
-    conin=input("The final name is "+final_name + ". Do you want to continue, or change the name? (y/n) : ")
-    if conin=="y":
+    conin=input("The final name is "+final_name + ". Do you want to continue, or change the name? (Y/n) : ")
+    if conin=="y" or conin=="":
         return
     elif conin=="n":
         get_input()
@@ -29,18 +29,28 @@ def get_input():
         save_dir=getcwd()
         save_dir_empty=True
     final_name=name
-    con()
+    con() # Asks to confirm the name.
 
-get_input()
+get_input() # Gets the name and save directory, and confirms the name.
 print("Getting title count...")
+
+# Gets the info of the disk, which lets us get title count.
 info=m.info()
+
 print("Title count is "+ str(info["title_count"]))
 
+# This will make sure RippedMedia exists, and if it does and errors, just pass.
 try: mkdir(save_dir+"/"+"RippedMedia")
-except:pass
+except:pass 
+
+# Makes sure the final save directory exists, and if it does and errors, just print it becuase of a potential error.
 try: mkdir(save_dir+"/"+"RippedMedia" +"/"+final_name)
 except:print("Directory already found. Warning that if you don't delete already ripped tracks in there it will try to overwrite them and error.")
+
 for i in range(info["title_count"]):
+
     print("Ripping title " + str(i))
-    m.mkv(i,save_dir+"/"+"RippedMedia" +"/"+final_name)
+
+    m.mkv(i,save_dir+"/"+"RippedMedia" +"/"+final_name) # Rip title i to MKV.
+
 print("...Ripping done.")
